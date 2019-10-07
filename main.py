ff36b468a10c2ebb1875db1cc1157a200e3d6700
thibaut boissin <thibaut.boissin@gmail.com>
2019-10-07 15:31:06 +0200

    [Architecture][#1] changed map to python arrays

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import sys
+import numpy as np
+from enum import IntEnum
 
 
 class Entity:
@@ -27,16 +29,46 @@ class Robot(Entity):
     """
     Entity that will seek for amadeusium
     """
+    class Task(IntEnum):
+        AVAILABLE = 0
+        PLACE_RADAR = 1
+        RADAR = 2
 
     def __init__(self, x, y, item):
         super().__init__(x, y, item)
         self.action = 'WAIT'
+        self.task = Robot.Task.AVAILABLE
 
     def update(self, x, y, item):
         super().update(x, y, item)
 
+    def radar(self):
+        if self.task == Robot.Task.RADAR:
+            self.action = 'REQUEST RADAR'
+            if self.item == 2:
+                self.task = Robot.Task.PLACE_RADAR
+        if self.task == Robot.Task.PLACE_RADAR:
+            self.action = 'DIG %s %s' % self.target
+            if self.item != 2:
+                self.task = Robot.Task.AVAILABLE
+                self.action = 'WAIT'
+
+        self.action += ' RADAR'
+
     def play(self):
+        """
+        Output the robot action for the current round
+        """
+        if self.task == Robot.Task.AVAILABLE:
+            self.action = 'WAIT'
+        elif self.task <= Robot.Task.RADAR:
+            self.radar()
+
         print(self.action)
+
+    def get_task(self, task, target):
+        self.task = task
+        self.target = target
 
 
 class Radar(Entity):
@@ -69,39 +101,58 @@ def main():
 
     # Get map dimensions
     width, height = [int(i) for i in input().split()]
-    map = [[('?', '0')] * width] * height
+    ore = np.zeros((height, width), dtype=np.int8)
+    hole = np.zeros((height, width), dtype=np.bool)
     entities = dict()
+    entity_cnt, radar_cd, trap_cd = 0, 0, 0
     allies = set()
-    ores = []
+    radar = Radar(5, 5, -1)
+    radar_busy = False
 
     while True:
         # Get the score
-        scores = [int(i) for i in input().split()]
+        myScore, enemyScore = [int(i) for i in input().split()]
 
         # Get the map
         for i in range(height):
-            inputs = input().split()
-            for j in range(width):
-                state = (inputs[2*j], inputs[2*j+1])
-                if state[0] not in ['?', '0']:
-                    ores.append((j, i))
-                map[i][j] = state
+            row = np.vectorize(np.uint8)(
+                    input().replace('?', '-1')  # replace ? in the string
+                        .split()  # convert to to array
+                )  # convert this to int
+            ore[i, :] = row[0::2]  # 1 over 2 element starting at 0
+            hole[i, :] = row[1::2]  # 1 over 2 elements starting at 1
 
         # Get entity
-        entity_count, radar_cd, trap_cd = [int(i) for i in input().split()]
-        for i in range(entity_count):
+        entity_cnt, radar_cd, trap_cd = np.vectorize(np.uint8)(input().split())
+        for i in range(entity_cnt):
             id, type, x, y, item = [int(j) for j in input().split()]
             try:
                 entities[id].update(x, y, item)
             except KeyError:
                 entities[id] = entity_factory[type](x, y, item)
-            if type == 0:
-                allies.add(id)
+                if type == 0:
+                    allies.add(id)
 
-        # AI here
+        # Create tasks
+        tasks = []
+        if radar_cd == 0 and not radar_busy:
+            radar_busy = True
+            tasks.append((Robot.Task.RADAR, (radar.x, radar.y)))
+            radar.x = (radar.x + 4) % width
+            radar.y = (radar.y + 4) % height
+        elif radar_cd != 0:
+            radar_busy = False
 
+        # Give task
         for ally in allies:
             unit = entities[ally]
+            if unit.task == Robot.Task.AVAILABLE:
+                try:
+                    # Decompose tuple in multiple args
+                    unit.get_task(*tasks.pop())
+                except IndexError:
+                    pass
+
             unit.play()
 
 
